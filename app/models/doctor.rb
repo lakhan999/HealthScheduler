f208341f7d25cad2b75e71208c4efc560507f219
lakhan999 <lakhanparmar07052000@gmail.com>
2024-09-05 12:47:30 +0530

    model validations and generation of rspac for patients and doctors model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,6 @@
 class Doctor < ApplicationRecord
   has_many :appointments
   has_many :patients, through: :appointments
+  validates :name, :mobile_number, :specialization, :consultation_fees, presence: true
+  validates :mobile_number, numericality: true, length: { in: 0..10 }
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,6 @@
 class Patient < ApplicationRecord
   has_many :appointments
   has_many :doctors, through: :appointments
+  validates :name, :mobile_number, :blood_group, presence: true
+  validates :mobile_number, numericality: { only_integer: true }, length: { in: 0..10 }
 end
